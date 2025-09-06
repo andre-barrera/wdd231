4deecf13c25a6390f676a68790c338cb49ffc859
andre-barrera <andre.barrera.99@gmail.com>
2025-09-06 13:45:58 -0600

    updated changes

--- week01/scripts/navigation.js
+++ week01/scripts/navigation.js
@@ -1,5 +1,13 @@
 const navbutton = document.querySelector('#ham-btn');
 
+// Navigation menu
+const navBar = document.querySelector('#nav-bar');
+
+
 navbutton.addEventListener('click', () => {
     navbutton.classList.toggle('show');
+    navBar.classList.toggle('show');
 });
+
+
+
